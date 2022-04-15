638bb6625bf7be5cc30257f4af1563b654e0fc5e
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-04-15 20:08:31 +0600

    header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
-import Experts from './Pages/Home/Experts/Experts';
 import Home from './Pages/Home/Home/Home';
 import ServiceDetail from './Pages/Home/ServiceDetail/ServiceDetail';
-import Services from './Pages/Home/Services/Services';
 import Login from './Pages/Login/Login/Login';
 import Footer from './Pages/Share/Footer/Footer';
 import Header from './Pages/Share/Header/Header';
@@ -18,9 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/home/services" element={<Services />} />
         <Route path="service/:serviceId" element={<ServiceDetail />} />
-        <Route path="/home/experts" element={<Experts />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path='*' element={<NotFound/>}></Route>
--- src/Pages/Share/Header/Header.jsx
+++ src/Pages/Share/Header/Header.jsx
@@ -12,8 +12,8 @@ const Header = () => {
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link as={Link} to="#services">Services</Nav.Link>
-      <Nav.Link as={Link} to="#experts">Experts</Nav.Link> 
+      <Nav.Link  href="home#services">Services</Nav.Link>
+      <Nav.Link href="home#experts">Experts</Nav.Link> 
     </Nav>
     <Nav>
       <Nav.Link as={Link} to="/about">About</Nav.Link>
