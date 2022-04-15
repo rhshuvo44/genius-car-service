313e2a12cfe1b867e752a5bcdc9bcfcf4db6e615
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-04-15 19:15:21 +0600

    not found pages

--- src/Pages/Share/NotFound/NotFound.js
+++ src/Pages/Share/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='container'>
-            
+            <h3 className='text-danger'>404</h3>
         </div>
     );
 };
