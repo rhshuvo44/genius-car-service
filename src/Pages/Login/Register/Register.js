4ca5469449c0755493a1a28123f25110848590bc
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-04-16 14:59:47 +0600

    firebase

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,21 +3,42 @@ import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Register = () => {
-    const handleRegister=(e)=>{
-        e.preventDefault();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+    if (!password === confirmPassword) {
+      return;
     }
+  };
   return (
     <div className="container py-5">
       <h2>Please Register</h2>
-      <Form className='w-50 mx-auto' onSubmit={handleRegister} style={{ textAlign: "left" }}>
+      <Form
+        className="w-50 mx-auto"
+        onSubmit={handleRegister}
+        style={{ textAlign: "left" }}
+      >
         <Form.Group className="mb-3" controlId="formBasicName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your full name" required/>
-          
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter your full name"
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required/>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
           <Form.Text className="text-muted">
             We'll never share your email with anyone else.
           </Form.Text>
@@ -27,14 +48,18 @@ const Register = () => {
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            placeholder="Password" required
+            name="password"
+            placeholder="Password"
+            required
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
             type="password"
-            placeholder="Confirm Password" required
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            required
           />
         </Form.Group>
         <Button variant="primary" type="submit">
